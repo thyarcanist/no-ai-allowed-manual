7b160209b183117c47f8dbe322c002642798fcfe
Datorien Anderson <48234494+thyarcanist@users.noreply.github.com>
2026-02-11 18:02:32 -0500

    Add RatesOfChange class for growth rate calculations

---
+++ shots/RatesOfChange.cs
@@ -0,0 +1,54 @@
+using System;
+					
+public class Program
+{
+	public static bool _isLinear = false;
+	public static float _selectedRate = .24f;
+	public static float currentRate = 0f;
+	public static float limitOfRate = 7.25f;
+	public static float baseRateChange = 1.25f;
+	public static int totalCountOfChanges = 0;
+	
+	public static void Main()
+	{
+		Console.WriteLine("Choose a growth style, and rate.");
+		SelectYourGrowth(_isLinear, _selectedRate);
+	}
+	
+	
+	public static void SelectYourGrowth(bool isLinear, float selectedRate) {
+			if (isLinear) {
+				RateOfLinearChange(selectedRate);
+			} else {
+				RateOfExponentialChange(selectedRate);	
+			}
+	}
+	
+		// This is a linear growth rate
+	public static void RateOfLinearChange(float rate){	
+		while (currentRate < limitOfRate) {
+			currentRate += rate * baseRateChange;
+			totalCountOfChanges += totalCountOfChanges + 1;
+			Console.WriteLine($"Rate is: {currentRate}");
+			if (currentRate >= limitOfRate) {
+				Console.WriteLine($"Rate is has been met. It took {totalCountOfChanges} to reach this point.");
+				totalCountOfChanges = 0;
+				break;	
+			}
+		}
+	}
+	
+		// This is a exponential growth rate - fixing
+	public static void RateOfExponentialChange(float rate){	
+		while (currentRate < limitOfRate) {
+			currentRate *= baseRateChange;
+			totalCountOfChanges += totalCountOfChanges + 1;
+			Console.WriteLine($"Rate is: {currentRate}");
+			if (currentRate >= limitOfRate) {
+				Console.WriteLine($"Rate is has been met. It took {totalCountOfChanges} to reach this point.");
+				totalCountOfChanges = 0;
+				break;	
+			}
+		}
+	}
+}
