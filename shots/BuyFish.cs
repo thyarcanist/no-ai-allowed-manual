9a2edd4ce001ba22f3ba665b4b575e030c430a32
Datorien Anderson <48234494+thyarcanist@users.noreply.github.com>
2026-02-11 01:01:07 -0500

    Refactor fish price calculation and add inventory
    Added fishMultiplier for price calculation and initialized inventory.

--- shots/BuyFish.cs
+++ shots/BuyFish.cs
@@ -14,11 +14,13 @@ public class Program
     public static string[] fishNames;
 	
 	public static double basePrice = 7.25f;
+	public static double fishMultiplier = 1.25;
 	
 	public static double[] fishPrices;
 	
 	// user
 	public static double wallet;
+	public static FishType[] inventory;
 	
 	
 	
@@ -28,6 +30,7 @@ public class Program
 		// the user will 
 		fishNames = new string[fishCount];
 		fishPrices = new double[5];
+		inventory = new FishType[0];
 		AssignFish();
 		
 		Console.WriteLine("Wanna buy some fish? \n");
@@ -50,13 +53,13 @@ public class Program
 		Console.WriteLine("Here's the list of fish we've got for sale. \n");
 		for (int i = 0; i < fishCount; i++) {
 			fishNames[i] = Enum.GetValues(typeof(FishType)).GetValue(i).ToString();	
-			fishPrices[i] = currentPrice *= 1.25;
+			fishPrices[i] = currentPrice *= fishMultiplier;
 			Console.WriteLine($"{fishNames[i]} is ${fishPrices[i]:F2}.");
 		}
 		Console.WriteLine("\n");
 	}
 	
-	public void PurchaseCount(int o, int f){
+	public void PurchaseCount(int quantity, FishType fish){
 		// what to do when buying?
 	}
 	
