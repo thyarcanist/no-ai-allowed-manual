fb2512778f4b13ff3113efc354f4e0bca3e08cd6
Datorien Anderson <48234494+thyarcanist@users.noreply.github.com>
2026-02-11 20:07:51 -0500

    Add character parser in Test.cs
    Implemented a character parser for a given string in the Main method. This is nonfunctional atm

---
+++ shots/Test.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Program
+{
+
+  // Day 3 program, char parser for a given string
+
+  public static string word = "owbwvyyeuhdownxoruentnxbsowirurnfbsyanebgjhsbevxi";
+
+   public static void Main() {
+
+
+       // i want to check each char in the string.
+     
+       for (int i = 0; i < word.Length(); i++)
+       {
+            foreach (char in word)
+            {
+
+            }
+       }
+     
+   }
+
+
+
+}
