0c5717b8f9a5ed731d7ac13d1480e3b4260630cb
Datorien Anderson <48234494+thyarcanist@users.noreply.github.com>
2026-02-11 20:48:29 -0500

    Refactor char counting and add print methods
    Refactored character counting logic and removed redundant char array initialization. Added methods for printing characters from end to front and front to end.

---
+++ shots/CharCount.cs
@@ -0,0 +1,51 @@
+using System;
+
+public class Program
+{
+
+  // Day 3 program, char parser for a given string
+
+  public static string word = "owbwvyyeuhdownxoruentnxbsowirurnfbsyanebgjhsbevxi";
+  public static char[] charValues = word.ToCharArray();
+
+   public static void Main() {
+		
+	   int wCount = 0;
+	   int rCount = 0;
+	   int otherChars = 0;
+	   int totalCharCount = word.Length;
+	   
+       // EACH CHAR in STRING without using ToCharArray();	   
+	   foreach (char o in word){
+			// Console.WriteLine($"{o}");
+		   if (o == 'w'){
+				wCount++;   
+		   } else if (o == 'r') {
+		   		rCount++;
+		   } else {
+				otherChars++;   
+		   }
+	   }
+	   
+	   Console.WriteLine($"There are: {wCount} of 'w'.\n There are: {rCount} of 'r'.\n Every other character is {otherChars} in count.\n ");
+	   Console.WriteLine($"There are {totalCharCount} of char's total.");
+     
+   }
+	   
+	   // i want to check each char in the string; solved. but not for a more manual approach?
+	   // initial bug for this that was solved, needed to but >= 0 because it needs to reach the end of the string.
+	   // holding these patterns for use later
+	   public static void EndToFront() {
+		for (int i = charValues.Length - 1; i >= 0; i--){
+			Console.WriteLine($"{i} = {charValues[i]}");
+		}
+	   }
+	   
+	   public static void FrontToEnd(){
+		for (int i = 0; i < charValues.Length; i++) {
+        	Console.WriteLine($"{i} = {charValues[i]}");
+       	}
+	   }
+     
+   }
+	
