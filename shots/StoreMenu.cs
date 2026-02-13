7109ee654298a0e2b42e1be5f3d25e09ce90f82d
Datorien Anderson <48234494+thyarcanist@users.noreply.github.com>
2026-02-12 19:55:18 -0500

    Add StoreMenu class with menu item management
    Implemented a store menu with menu items and methods to initialize and retrieve them by ID.

---
+++ shots/StoreMenu.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+					
+public class Program
+{
+
+  	// wrong pattern - useful for lookups only public static Dictionary<string,double> storeMenu = new Dictionary<string,double>();
+	public static List<MenuItem> storeMenu = new List<MenuItem>();	// make a list of that new class of item
+
+  // needed to make a class because dictionaries and a normal list what's correct.
+	public class MenuItem {
+		public int ID;
+		public string NAME;
+		public double PRICE;
+	}
+	
+		public static void InitializeStoreMenu() {
+		
+		storeMenu.Add(new MenuItem { ID = 1, NAME = "Spring Rolls", PRICE = 4.75 });
+		storeMenu.Add(new MenuItem { ID = 2, NAME = "Double Cheeseburger", PRICE = 5.25 });
+		storeMenu.Add(new MenuItem { ID = 3, NAME = "Beef Shishkabob", PRICE = 8.25 });
+    	storeMenu.Add(new MenuItem { ID = 3, NAME = "California Roll", PRICE = 16.25 });
+	}
+
+  // Had to write a method to fetch the MenuItem by the ID and return it.
+	public static string GetMenuItemByID(int id){
+		return storeMenu.Find(MenuItem => MenuItem.ID == id).NAME;
+	}
+	
+	public static void Main() {
+	
+		InitializeStoreMenu();
+
+    // It should print the beef shishkabob
+		Console.WriteLine($"Item number 3 is {GetMenuItemByID(3)}.");
+
+	}
+	
+
+}
